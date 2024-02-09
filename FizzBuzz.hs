3c807b88f39d4476e8d481fffe6b68edd16829f4
Pedro Antonio Ayala Ramos <48796502+ElPedrangas03@users.noreply.github.com>
2024-02-08 19:50:09 -0600

    Comentarios FizzBuzz.hs

--- FizzBuzz.hs
+++ FizzBuzz.hs
@@ -1,15 +1,24 @@
+-- Metodo que recibe un entero y regresa su equivalente en texto, esto solo para los numeros de 1 a 20
+-- Entrada: 4
+-- Salida: four
 lessThan20 :: Int -> String
 lessThan20 n |
     n > 0 && n < 20 =
     let answers = words ("one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeeb eighteen nineteen")
     in answers !! (n-1)
 
+-- Metodo que recibe un entero y regresa el valor de cada 10
+-- Entrada: 20
+-- Salida: twenty
 teens :: Int -> String
 teens n | n >= 2 && n <= 9 =
     answers !! (n-2)
     where
         answers = words("twenty thirty forty fifty sixty seventy eighty ninety")
 
+-- Metodo general para calculas el FizzBuzz
+-- Entrada: 15
+-- Salida: FizzBuzz
 number :: Int -> String
 number n 
     | n `mod` 3 == 0 && n `mod` 5 == 0 = "FizzBuzz" 
