fdca35aac104473fde7e994e2df41bf0cb001a35
Pedro Antonio Ayala Ramos <48796502+ElPedrangas03@users.noreply.github.com>
2024-02-08 19:39:36 -0600

    Actualizacion Tarea 1.hs

--- Tarea 1.hs
+++ Tarea 1.hs
@@ -3,38 +3,52 @@ import Data.List (foldl')
 -- Parte 1 de la tarea
 
 -- Función para aplicar un descuento a un precio
+-- Entrada: 100 30
+-- Salida: 70
 aplicarDescuento :: Double -> Double -> Double
 aplicarDescuento precio descuento = precio - (precio * (descuento/100))
 
 -- Función para aplicar el IVA a un precio
-aplicarIVA :: Double -> Double
-aplicarIVA precio = precio * 1.16
+-- Entrada: 100 16
+-- Salida: 116
+aplicarIVA :: Double -> Double -> Double
+aplicarIVA precio descuento = precio + (precio * (descuento/100))
 
 -- Función que recibe un diccionario de precios y porcentajes y una función para aplicar descuentos o IVA
+-- Entrada: [(100, 30), (200, 25)] aplicarDescuento
+-- Salida: 220
 calcularPrecioFinal :: [(Float, Float)] -> (Float -> Float -> Float) -> Float
 calcularPrecioFinal basket function = sum [function price discount | (price, discount) <- basket]
 
 -- Parte 2 de la tarea
 
--- Definición de la función aplicaFuncionLista
+-- Función para aplicar una función a cada elemento de una lista
+-- Entrada: (cuadrado, [1, 2, 3, 4])
+-- Salida: [1, 4, 9, 16]
 aplicaFuncionLista :: (a -> b) -> [a] -> [b]
-aplicaFuncionLista _ []     = []             -- Caso base: lista vacía
--- Aplicar la función a cada elemento y recursión sobre el resto de la lista
+aplicaFuncionLista _ []     = []             
 aplicaFuncionLista f (x:xs) = f x : aplicaFuncionLista f xs  
 
--- Definición de la función cuadrado
+-- Función para calcular el cuadrado de un número
+-- Entrada: 3
+-- Salida: 9
 cuadrado :: Num a => a -> a
 cuadrado n = n * n
 
 -- Parte 3 de la tarea
 
--- Partimos la palabra que nos den y regresamos las palabras y longitud de cada una
+-- Función para dividir una cadena de texto en palabras y devolver una lista de tuplas con cada palabra y su longitud
+-- Entrada: "Hola mundo"
+-- Salida: [("Hola", 4), ("mundo", 5)]
 partirPalabras :: String -> [(String, Int)]
 partirPalabras salida = map (\w -> (w, length w)) (words salida)
 
+
 -- Parte 4 de la tarea
 
--- Convertimos lo que tengamos de calificacion al texto correspondiente
+-- Función para convertir una calificación numérica en un texto descriptivo
+-- Entrada: 85
+-- Salida: "Notable"
 convertirCalificacion :: Float -> String
 convertirCalificacion nota
     |   nota >= 95 = "Excelente"
@@ -43,29 +57,27 @@ convertirCalificacion nota
     |   nota >= 70 = "Suficiente"
     |   otherwise = "Desempeño insuficiente"
 
--- Regresamos la coleccion con las asignaturas en mayusculas y su respectiva calificacion
+-- Función para convertir las calificaciones de una lista de tuplas de (asignatura, nota) a (asignatura en mayúsculas, calificación)
+-- Entrada: [("Matemáticas", 80), ("Física", 90)]
+-- Salida: [("MATEMÁTICAS", "Notable"), ("FÍSICA", "Excelente")]
 calificaciones :: [(String, Float)] -> [(String, String)]
 calificaciones = map (\(asignatura, nota) -> (map toUpper asignatura, convertirCalificacion nota))
 
 -- Parte 5 de la tarea
 
--- Tomamos el modulo de un vector dado
+-- Función para calcular el módulo de un vector
+-- Entrada: [3, 4]
+-- Salida: 5.0
 moduloDeVector :: Floating a => [a] -> a
 moduloDeVector = sqrt . sum . map (^2)
 
 -- Parte 6 de la tarea
 
--- Función para calcular la media de una lista de números
-media :: (Fractional a) => [a] -> a
-media xs = sum xs / fromIntegral (length xs)
-
--- Función para calcular la desviación estándar de una lista de números
-desviacionEstandar :: (Floating a) => [a] -> a
-desviacionEstandar xs =
-  let m = media xs
-      n = fromIntegral $ length xs
-      squaredDiffs = map (\x -> (x - m) ^ 2) xs
-  in sqrt (sum squaredDiffs / n)
+-- Función para filtrar los valores atípicos en una muestra
+-- Entrada: [1, 2, 3, 4, 5, 1000]
+-- Salida: [1000]
+datosAtipicos :: (Floating a, Ord a) => [a] -> [a]
+datosAtipicos muestra = filter (atipico muestra) muestra
 
 -- Función para determinar si un valor es atípico en función de la media y la desviación estándar
 atipico :: (Floating a, Ord a) => [a] -> a -> Bool
@@ -75,6 +87,18 @@ atipico muestra n =
       puntuacion = (n - mediaMuestra) / desviacion
   in puntuacion < -3 || puntuacion > 3
 
--- Función para filtrar los datos atípicos de una muestra
-datosAtipicos :: (Floating a, Ord a) => [a] -> [a]
-datosAtipicos muestra = filter (atipico muestra) muestra
+-- Función para calcular la media de una lista de números
+-- Entrada: [1, 2, 3, 4, 5]
+-- Salida: 3.0
+media :: (Fractional a) => [a] -> a
+media xs = sum xs / fromIntegral (length xs)
+
+-- Función para calcular la desviación estándar de una lista de números
+-- Entrada: [1, 2, 3, 4, 5]
+-- Salida: 1.4142135623730951
+desviacionEstandar :: (Floating a) => [a] -> a
+desviacionEstandar xs =
+  let m = media xs
+      n = fromIntegral $ length xs
+      squaredDiffs = map (\x -> (x - m) ^ 2) xs
+  in sqrt (sum squaredDiffs / n)
